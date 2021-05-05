d7b04da9a66189bd2e41ae8a456968e71e7ec087
ds <19520216@gm.uit.edu.vn>
2021-05-05 23:40:41 +0700

    Update Splash Screen animate 01

--- app/src/main/java/com/example/thecoffeeshopmanagement/LoginActivity.java
+++ app/src/main/java/com/example/thecoffeeshopmanagement/LoginActivity.java
@@ -6,14 +6,18 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
         ConstraintLayout con = (ConstraintLayout)findViewById(R.id.login_layout);
         con.setBackgroundColor(Color.WHITE);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
     }
+
 }
